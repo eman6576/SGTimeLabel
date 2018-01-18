a8786a5102b1a7a27a494baa92acba386e04bdce
Emanuel Guerrero <emanuelguerrerodev@gmail.com>
2018-01-18 12:18:21 -0500

    Initial commit

---
+++ SGTimeLabel-Sample/SGTimeLabel-Sample/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  SGTimeLabel-Sample
+//
+//  Created by Emanuel  Guerrero on 1/18/18.
+//  Copyright © 2018 SwiftyGuerrero. All rights reserved.
+//
+
+import SGTimeLabel
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ SGTimeLabel/SGTimeLabel/SGTimeLabel.h
@@ -0,0 +1,19 @@
+//
+//  SGTimeLabel.h
+//  SGTimeLabel
+//
+//  Created by Emanuel  Guerrero on 1/18/18.
+//  Copyright © 2018 SwiftyGuerrero. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SGTimeLabel.
+FOUNDATION_EXPORT double SGTimeLabelVersionNumber;
+
+//! Project version string for SGTimeLabel.
+FOUNDATION_EXPORT const unsigned char SGTimeLabelVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SGTimeLabel/PublicHeader.h>
+
+
